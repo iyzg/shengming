2c74ef2dcb48448e0ce5b759a334b2b6ee699c09
Ivy Zhang <ZIvy042003@gmail.com>
2021-04-11 19:03:17 -0400

    doot

---
+++ shengming.py
@@ -0,0 +1,77 @@
+import sys
+
+def time_to_minutes(time):
+    # TODO: Convert time to minutes
+    split_time = time.split(":")
+    return int(split_time[0]) * 60 + int(split_time[1])
+
+
+
+def main():
+    # TODO: Add way for scores / 30 minutes
+    # TODO: Happiness every day
+    log = None
+    tags = []
+    tagTime = {}
+
+    lastTime = 0
+
+    try:
+        log = open("daily", "r")
+    except IOError:
+        print("No daily file found")
+        sys.exit();
+
+    lines = log.readlines()
+
+    for line in lines:
+        words = line.split()
+
+        # TODO: Handle subtag
+        # Reset when you're on dates
+        if len(words) == 0:
+            continue;
+        elif len(words) == 1:
+            lastTime = 0
+            tags.clear()
+            continue
+
+        if not words[0][0].isdigit():
+            continue;
+
+        time_passed = time_to_minutes(words[0])
+        # Process last one
+        if lastTime != 0:
+            for tag in tags:
+                temp_tags = []
+                tag_split = tag.split("(")
+                for section in tag_split:
+                    if ')' in section:
+                        temp_tags.append(tag_split[0] + " >> " + section[:-1])
+                    else:
+                        temp_tags.append(section)
+
+
+                for ttag in temp_tags:
+                    if ttag not in tagTime:
+                        tagTime[ttag] = time_passed - lastTime
+                    else:
+                        tagTime[ttag] += time_passed - lastTime
+
+        # Push Current (Not if FIN)
+        tags.clear()
+        if words[1] == "FIN":
+            continue
+
+        lastTime = time_passed
+        for word in words:
+            if word[0] == '@':
+                tags.append(word[1:])
+
+    for tag in sorted(tagTime):
+        if ">>" not in tag:
+            print()
+        print("{}: {} minutes".format(tag, tagTime[tag]))
+
+if __name__ == '__main__':
+    main()
